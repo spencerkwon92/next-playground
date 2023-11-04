fc1c553c09291ca551730c9c688066d1a6077e36
Spencer Kwon <spencerkwon92@gmail.com>
2023-11-05 02:48:09 +0900

    adding react-query.

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,20 +1,42 @@
-import React from 'react'
-import { AppProps, NextWebVitalsMetric } from "next/app";
-import Head from 'next/head'
+import "@/styles/globals.css";
+import "daisyui/dist/full.css";
+import {useRef} from "react";
+import { NextWebVitalsMetric } from "next/app";
+import Head from "next/head";
+import {QueryClient, QueryClientProvider, Hydrate} from "react-query";
+import { DehydratedState } from "react-query/hydration";
+import {ReactQueryDevtools} from "react-query/devtools";
+import PropTypes from "prop-types";
 
-function App({Component, pageProps}: AppProps){
+import { MyAppProps } from "@/interface/AppProps";
+
+const App = ({ Component, pageProps }: MyAppProps) => {
+  const queryClientRef = useRef<QueryClient>();
+  if (!queryClientRef.current) {
+    queryClientRef.current = new QueryClient();
+  }
 
   return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.ico" />
-        <title>Next Playground</title>
-      </Head>
-      <Component {...pageProps} />
-    </>
+    <QueryClientProvider client={queryClientRef.current}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <Head>
+          <meta charSet="utf-8" />
+          <link rel="icon" href="/favicon.ico" />
+          <title>Next Playground</title>
+        </Head>
+        <Component {...pageProps} />
+        <ReactQueryDevtools initialIsOpen={false} />
+      </Hydrate>
+    </QueryClientProvider>
   );
-  
+};
+
+App.propTypes = {
+  Component: PropTypes.elementType.isRequired,
+};
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  console.log(metric);
 }
 
 export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,21 @@
-import React from 'react';
+import React from "react";
+import AppLayout from "../components/layout/AppLayout";
+import {useQuery, useQueryClient} from "react-query";
 
-function Home() {
+import { loadMyInfoAPI } from "@/apis/user";
+
+const Home = () => {
+  const { data: me } = useQuery("user", loadMyInfoAPI);
+  const queryClient = useQueryClient();
 
   return (
-    <div>
-      This is HomePage.
-    </div>
-  ) 
-}
+    <AppLayout>
+      <div className="grid grid-cols-6 gap-3">
+        <div className="col-span-4">postSection</div>
+        <div className="col-span-2">loginSection</div>
+      </div>
+    </AppLayout>
+  );
+};
 
 export default Home;
